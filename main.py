e696e1d3eff78239857fbde790a3db39a395988d
Owen Leonard <oleonard15@gmail.com>
2023-12-29 00:38:07 -0500

    complete parsing and scale for many images

--- main.py
+++ main.py
@@ -12,50 +12,71 @@ if api_key is None:
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
+  
+# Parse the API response
+def parse_response(response):
+    res = response.json()["choices"][0]["message"]["content"]
+    # remove json from beginning of string
+    res = res[5:]
+    # remove ''' from beginning and end of string
+    res = res[3:-3]
+    return res
 
-# Path to your image
-image_path = "data/-ca8b-4b35-999a-611f48abebcc2F0ce3330e-54e7-4d07-a065-f245e1dbd2472FUntitled.png"
 
-# Getting the base64 string
-base64_image = encode_image(image_path)
+# accepts image file path and returns json string of json object
+def parse_image(image_path):
+    # Path to your image
+    image_path = "data/-ca8b-4b35-999a-611f48abebcc2F0ce3330e-54e7-4d07-a065-f245e1dbd2472FUntitled.png"
 
-headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {api_key}"
-}
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
 
-payload = {
-  "model": "gpt-4-vision-preview",
-  "messages": [
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "What are the top 5 artists, top 5 songs, minutes listened, \
-            and top genre from this spotify wrapped photo? please return in json \
-            format in the order listed above. If any values are missing return null for that value."
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}",
-            "detail": "low"
-          }
-        }
-      ]
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
     }
-  ],
-  "max_tokens": 300
-}
 
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": "What are the top 5 artists, top 5 songs, minutes listened, \
+                and top genre from this spotify wrapped photo? please return in json \
+                format in the order listed above. If any values are missing return null for that value."
+            },
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}",
+                "detail": "low"
+            }
+            }
+        ]
+        }
+    ],
+    "max_tokens": 300
+    }
 
-# print(response.json())
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    return parse_response(response)
 
-# write response to json file
-with open('response.json', 'w') as f:
-  f.write(response.text)
 
-print(response.json()["choices"][0]["message"]["content"])
+# entry point
+if __name__ == "__main__":
+    image_paths = os.listdir("data")
 
+    # parse each image
+    complete_json = ""
+    for path in image_paths:
+        complete_json += parse_image(path) + ","
+    
+    # remove last comma
+    complete_json = complete_json[:-1]
+
+    # write to file
+    with open('response.json', 'w') as f:
+        f.write("["+complete_json+"]")
