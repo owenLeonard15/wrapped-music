108450cff5f9b19d64da4e7d373e0bfbc722eb03
Owen Leonard <oleonard15@gmail.com>
2023-12-30 18:09:43 -0500

    add rate limiting, thorough error catching, json formatting

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
 import base64
 import requests
 import os
 from tqdm.auto import tqdm
+
+# current rate limits: https://platform.openai.com/account/limits
 
 # get API key from environment
 api_key = os.getenv("OPENAI_API_KEY")
@@ -17,14 +20,21 @@ def encode_image(image_path):
 # Parse the API response
 def parse_response(response):
     res = response.json()["choices"][0]["message"]["content"]
-    # remove json from beginning of string
-    res = res[5:]
+    
     # remove ''' from beginning and end of string
-    res = res[3:-3]
+    # res = res[1:-1]
+
     # remove any newlines from the end of the string
     res = res.rstrip()
     return res
 
+# Get the number of tokens left from the API response
+# available tokens are returned in response header under x-ratelimit-limit-tokens
+def tokens_remaining(response):
+    return response.headers['x-ratelimit-remaining-tokens']
+
+def time_until_tokens_reset(response):
+    return response.headers['x-ratelimit-reset-tokens']
 
 # accepts image file paths and returns json string of json objects
 def parse_images(image_paths):
@@ -32,7 +42,20 @@ def parse_images(image_paths):
     base64_images = []
     # Getting the base64 string
     for image_path in image_paths:
+        # make sure image is one of the supported types: ['png', 'jpeg', 'gif', 'webp']
+        # take lowercase of last element in list
+        if image_path.split('.')[-1].lower() not in ['png', 'jpeg', 'jpg', 'gif', 'webp']:
+            print(f"Image {image_path} is not a supported type. Skipping...")
+            continue
+        
+        # make sure image is not too large (max 20MB)
+        if os.path.getsize("data/spotify/" + image_path) > 20000000:
+            print(f"Image {image_path} is too large. Skipping...")
+            continue
+
         base64_image = encode_image("data/spotify/" + image_path)
+
+
         base64_images.append(base64_image)
 
     # create payload including all base64 images
@@ -42,19 +65,21 @@ def parse_images(image_paths):
     }
 
     payload = {
-    "model": "gpt-4-vision-preview",
+    "model": "gpt-4-vision-preview",  
     "messages": [
         {
         "role": "user",
         "content": [
             {
             "type": "text",
-            "text": "What are the top 5 artists, top 5 songs, minutes listened, \
-and top genre from each of the following spotify wrapped photos? \
-Make guesses for incomplete values. Return a list of valid json objects. \
+            "text": "List the top 5 artists, top 5 songs, minutes listened, \
+and top genre from each of the following spotify wrapped photos. \
+Complete any incomplete songs or artists with the full title or artist name. \
+Return results in a list of valid json objects. \
 Each json object should contain a key value pair for each of top_artists, top_songs, minutes_listened, top_genre. \
 If any values are missing return null for that value. \
-If you cannot parse an image for any reason return an empty json object."
+Skip any images that cannot be parsed. \
+Do not include any additional text aside from the list of json objects."
             },
             # all base64 images
             *[{
@@ -67,16 +92,18 @@ If you cannot parse an image for any reason return an empty json object."
         ]
         }
     ],
-    "max_tokens": 300
+    "max_tokens": 4096
     }
 
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-    
+
     # check if response is valid
     if response.status_code != 200:
+        print(response.json())
         raise Exception("Response status code is not 200")
     
-    return parse_response(response)
+    
+    return (parse_response(response), tokens_remaining(response), time_until_tokens_reset(response))
 
 
 # entry point
@@ -84,14 +111,29 @@ if __name__ == "__main__":
     image_paths = os.listdir("data/spotify")
 
     parse_string = ""
-    # process 100 images at a time
-    for i in tqdm(range(0, len(image_paths), 100)):
-        # get 100 images
-        image_paths_100 = image_paths[i:i+100]
+    # process batch_count images at a time
+    batch_count = 10
+    for i in tqdm(range(0, len(image_paths), batch_count)):
+        # get batch_count images
+        batch_image_paths = image_paths[i:i+batch_count]
         # parse the images
-        parse_string = parse_images(image_paths_100)
+        try:
+            parsed_string, tokens_left, time_until_reset = parse_images(batch_image_paths)
+        except Exception as e:
+            print(e)
+            print("Skipping batch...")
+            # write skipped images to file:
+            with open(f"skipped_{i}.txt", 'w') as f:
+                f.write("\n".join(batch_image_paths))
+            continue
 
         with open(f"response_{i}.json", 'w') as f:
-            # remove last comma
-            parse_string = parse_string[:-1]
-            f.write("["+parse_string+"]")
+            f.write("["+parsed_string+"]")
+
+        print(f"Tokens used: {tokens_left}")
+        print(f"Time until tokens reset: {time_until_reset}")
+        # sleep for time_until_tokens_reset seconds plus 1 second to be safe
+        # example time_until_tokens_reset: '25.242s'
+        sleep_time = float(time_until_reset[:-1]) + 1
+        time.sleep(sleep_time)
+        
